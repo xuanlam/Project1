1f186728c98d1d113dfb6e0f4979566cc51927a9
Phi Long <philongsuphu@gmail.com>
2013-03-31 16:23:51 +0700

    Add Item on StoreMenuLayer

--- Pikachu/Pikachu/Classes/Scenes/StoreMenuLayer.m
+++ Pikachu/Pikachu/Classes/Scenes/StoreMenuLayer.m
@@ -41,18 +41,54 @@
         [self addChild:pauseButton];
         
         // ADD ITEM
+        // add shop wood 1
+        CCSprite *shopWood1 = [CCSprite spriteWithFile:@"b_0011_Shop_WOOD.png"];
+        shopWood1.anchorPoint = ccp(0.0f, 0.0f);
+        shopWood1.position = ccp(130.0f, 450.0f);
+        [self addChild:shopWood1];
         
+        // add list 5 shope piece pack on wood 1
+        CCMenuItemImage *pack1 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack1Sender)];
+        CCMenuItemImage *pack2 = [CCMenuItemImage itemWithNormalImage:@"b_0008_Shop_Piece_Pack_Buy.png" selectedImage:@"b_0008_Shop_Piece_Pack_Buy.png" disabledImage:@"b_0008_Shop_Piece_Pack_Buy.png" target:self selector:@selector(buttonPack1Sender)];
+        CCMenuItemImage *pack3 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack1Sender)];
+        CCMenuItemImage *pack4 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack1Sender)];
+        CCMenuItemImage *pack5 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack1Sender)];
+        CCMenu *packShop1 = [CCMenu menuWithItems:pack1, pack2, pack3, pack4, pack5, nil];
+        [packShop1 alignItemsHorizontallyWithPadding:20];
+        packShop1.position = ccp(520.0f, 545.0f);
+        [self addChild:packShop1];
+        
+        // add shop wood 2
+        CCSprite *shopWood2 = [CCSprite spriteWithFile:@"b_0011_Shop_WOOD.png"];
+        shopWood2.anchorPoint = ccp(0.0f, 0.0f);
+        shopWood2.position = ccp(130.0f, 150.0f);
+        [self addChild:shopWood2];
+        
+        // add list 5 shope piece pack on wood 2
+        CCMenuItemImage *pack6 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack2Sender)];
+        CCMenuItemImage *pack7 = [CCMenuItemImage itemWithNormalImage:@"b_0008_Shop_Piece_Pack_Buy.png" selectedImage:@"b_0008_Shop_Piece_Pack_Buy.png" disabledImage:@"b_0008_Shop_Piece_Pack_Buy.png" target:self selector:@selector(buttonPack2Sender)];
+        CCMenuItemImage *pack8 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack2Sender)];
+        CCMenuItemImage *pack9 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack2Sender)];
+        CCMenuItemImage *pack10 = [CCMenuItemImage itemWithNormalImage:@"b_0009_Shop_Piece_Pack.png" selectedImage:@"b_0009_Shop_Piece_Pack.png" disabledImage:@"b_0009_Shop_Piece_Pack.png" target:self selector:@selector(buttonPack2Sender)];
+        CCMenu *packShop2 = [CCMenu menuWithItems:pack6, pack7, pack8, pack9, pack10, nil];
+        [packShop2 alignItemsHorizontallyWithPadding:20];
+        packShop2.position = ccp(520.0f, 245.0f);
+        [self addChild:packShop2];
     }
     
     return self;
 }
 
-- (void)buttonBackSender {
-    
+- (void)buttonBackSender {    
 }
 
 - (void)buttonPauseSender {
-    
+}
+
+- (void)buttonPack1Sender {
+}
+
+- (void)buttonPack2Sender {
 }
 
 @end
