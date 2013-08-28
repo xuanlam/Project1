f54505a0228a187849fe7998bf9f9303127f9a0e
Mai Trieu Phi Long <maitrieuphilong@gmail.com>
2013-08-28 10:56:22 +0700

    add new music

---
+++ Pikachu/Pikachu/Classes/Support/NSMutableArray+Shuffling.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+Shuffling.h
+//  Pikachu
+//
+//  Created by Phi Long on 8/28/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (Shuffling)
+
+@end
---
+++ Pikachu/Pikachu/Classes/Support/NSMutableArray+Shuffling.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+Shuffling.m
+//  Pikachu
+//
+//  Created by Phi Long on 8/28/13.
+//
+//
+
+#import "NSMutableArray+Shuffling.h"
+
+@implementation NSMutableArray (Shuffling)
+
+@end
